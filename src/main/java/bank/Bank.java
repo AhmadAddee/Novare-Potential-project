1cad217b3872b0024ea1ad863de43210d3c3d62f
Marcus Forsberg <marcus.forsberg.dev@proton.me>
2022-06-27 16:26:12 +0200

    Hooked the login/sign-up to the UI so user register :rocket:

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -5,6 +5,12 @@ public class Bank {
 
     private final UserDB userDB = new UserDB();
 
+    /**
+     * A method for signing in to the system
+     * @param username The username of a user in the system.
+     * @param password The password for the user.
+     * @return A session object which is used for communicating different operations to the bank. Will return Empty if either username or password was wrong.
+     */
     public Optional<Session> signIn(String username, String password) {
         Optional<User> user = userDB.get(username);
 
@@ -14,9 +20,16 @@ public class Bank {
         return Optional.empty();
     }
 
+    /**
+     * A method for creating new users.
+     * @param username Username of the user.
+     * @param fullName the full name of the user.
+     * @param password the password which the user has chosen.
+     * @return True if the user was added to the system. False if username is already in use.
+     */
     public boolean signUp(String username, String fullName, String password){
-        boolean r = userDB.containsUser(username);
-        if(r)
+        boolean usernameAlreadyExists = userDB.containsUser(username);
+        if(usernameAlreadyExists)
             return false;
 
         User user = new User(username,fullName,password);
